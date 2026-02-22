158cdf430a2ed1ed9d1da109ed3645a29e214b9a
DeluxeViper <ab.tech.ventures@gmail.com>
2026-02-21 23:16:51 -0500

    Log healthz requests

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.http import JsonResponse
+
+logger = logging.getLogger(__name__)
 
 
 def healthz(request):
+    logger.info("healthz ok")
     return JsonResponse({"status": "ok"})
